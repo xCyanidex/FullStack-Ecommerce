f1bca3a5c9498d781c23c2ee60294aaf93259df7
Duraiz Haider <86344507+xCyanidex@users.noreply.github.com>
2023-11-23 13:24:41 +0500

    added stat cards

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,18 @@
-import ChartOne from '../Components/Charts/ChartOne'
+import CardFour from "../Components/Cards/CardFour";
+import CardOne from "../Components/Cards/CardOne";
+import CardThree from "../Components/Cards/CardThree";
+import CardTwo from "../Components/Cards/CardTwo";
+
 
 export default function Dashboard() {
   return (
-   <>
-  <ChartOne/>
-   </>
-  )
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        <CardOne/>
+        <CardTwo/>
+        <CardThree/>
+        <CardFour/>
+      </div>
+    </>
+  );
 }
